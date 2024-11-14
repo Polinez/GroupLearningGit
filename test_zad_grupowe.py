edc2c7179d3962e083629a090d4c9bd18c77d233
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-11-14 22:57:29 +0100

    feat: Import Unit tests

--- test_zad_grupowe.py
+++ test_zad_grupowe.py
@@ -1,20 +1,29 @@
+from unittest.mock import patch, mock_open
 import pytest
 from task1 import edit_students,import_students,add_student,remove_student,export_students,check_students
 
 class TestImportStudents:
+    @patch("builtins.open", mock_open(read_data="Jan Kowalski,Anna Nowak"))
+    def test_import_students_success(self):
+        #given
+        file_path = "students.txt"
 
-    #when
-    import_students()
+        # when
+        result = import_students(file_path)
+
+        # then
+        expected = {"Jan Kowalski": True, "Anna Nowak": True}
+        assert result == expected
 
 class TestAddStudent:
-
+    pass
     #when
-    add_student()
+    #add_student()
 
 class TestZaznaczObecnosc:
-
+    pass
     #when
-    check_students()
+    #check_students()
 
 class TestObecnosci:
     def test_edit_students(self):
@@ -39,12 +48,12 @@ class TestObecnosci:
         # Then
         assert students_list[imie] == False
 
-class TestDeleteStuden:
-
+class TestDeleteStudents:
+    pass
     #when
-    remove_student()
+    #remove_student()
 
 class TestExportStudents:
-
+    pass
     #when
-    export_students()
+    #export_students()
